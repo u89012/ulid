01afb29384d668d2616ac90477073a6d56140da7
Sunder Rajan <u89012@users.noreply.github.com>
2025-02-14 13:24:17 +0530

    Upgrade to Crystal v0.35.1

--- src/ulid.cr
+++ src/ulid.cr
@@ -4,10 +4,10 @@ module ULID
   extend self
 
   # Crockford's Base32
-  private ENCODING     = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
+  private ENCODING = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
   private ENCODING_LEN = ENCODING.size
-  private TIME_LEN     = 10
-  private RANDOM_LEN   = 16
+  private TIME_LEN = 10
+  private RANDOM_LEN = 16
 
   # Generate a ULID
   #
@@ -15,18 +15,18 @@ module ULID
   # ULID.generate
   # # => "01B3EAF48P97R8MP9WS6MHDTZ3"
   # ```
-  def generate(seed_time : Time = Time.now) : String
-    encode_time(seed_time, TIME_LEN) + encode_random(RANDOM_LEN)
+  def generate : String
+    encode_time(Time.utc, TIME_LEN) + encode_random(RANDOM_LEN)
   end
 
   private def encode_time(now : Time, len : Int32) : String
-    ms = now.epoch_ms
+    ms = now.to_unix_ms
 
     String.build do |io|
       len.times do |i|
         mod = ms % ENCODING_LEN
         io << ENCODING[mod]
-        ms = (ms - mod) / ENCODING_LEN
+        ms = (ms - mod) // ENCODING_LEN
       end
     end.reverse
   end
